380b2c27f6700ee5b730ace36bbf83f5030db36f
tsfeith-epfl <91938631+tsfeith-epfl@users.noreply.github.com>
2022-11-30 15:02:25 +0100

    Delete googletest directory

--- googletest/cmake-build-debug/lib/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tsfeith/OneDrive/Documentos/Education/EPFL_CSE/Courses/ProgrammingConcepts/ImageProcessing/Google_tests/lib/googletest
-# Build directory: /home/tsfeith/OneDrive/Documentos/Education/EPFL_CSE/Courses/ProgrammingConcepts/ImageProcessing/Google_tests/cmake-build-debug/lib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
