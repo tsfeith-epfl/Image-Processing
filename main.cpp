f840618220ac241d9304e0c0d512dc65fd48cf87
tsfeith-epfl <tomas.soaresdecarvalhofeith@epfl.ch>
2022-11-30 22:08:34 +0100

    CMakeLists not yet working
    It needs to be able to take the images from the source directory to the output directory but I haven't yet figured out how to do it

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
-#include "pcsc.hh"
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <fstream>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string>
+#include <filesystem>
 
-int main(int argc, char **argv) {}
+int main(int argc, char **argv) {
+    // test script for opencv
+    std::cout << std::filesystem::current_path() << std::endl;
+    std::ifstream f("./images/teapot.png");
+    std::cout << f.good() << std::endl;
+    cv::Mat img = cv::imread("./images/teapot.png");
+    cv::imshow("image", img);
+    cv::waitKey(0);
+    return 0;
+}
