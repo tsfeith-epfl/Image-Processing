58e5eaeb13e9a877d58ae839d2027b4715e8b394
tsfeith-epfl <tomas.soaresdecarvalhofeith@epfl.ch>
2022-12-03 17:35:47 +0100

    change test names to fit convention

--- test/convolutionsTests.cpp
+++ test/convolutionsTests.cpp
@@ -5,7 +5,7 @@
 /* Testing Operations.cpp */
 
 // fixture for testing Operations.cpp
-class applyConvolutionTest : public ::testing::Test
+class convolutionsTests : public ::testing::Test
 {
 protected:
     void SetUp() override
@@ -17,7 +17,7 @@ protected:
 };
 
 // test applyConvolution with a unit kernel
-TEST_F(applyConvolutionTest, applyConvolution_unitKernel)
+TEST_F(convolutionsTests, applyConvolution_unitKernel)
 {
     Eigen::ArrayXXd kernel = Eigen::ArrayXXd::Zero(3, 3);
     kernel(1, 1) = 1;
@@ -33,7 +33,7 @@ TEST_F(applyConvolutionTest, applyConvolution_unitKernel)
 }
 
 // test applyConvolution with a rectangular input
-TEST_F(applyConvolutionTest, applyConvolution_rectangularInput)
+TEST_F(convolutionsTests, applyConvolution_rectangularInput)
 {
     Eigen::ArrayXXd input = Eigen::ArrayXXd::Zero(5, 7);
     input(1, 1) = 1;
@@ -51,32 +51,32 @@ TEST_F(applyConvolutionTest, applyConvolution_rectangularInput)
 }
 
 // test errors
-TEST_F(applyConvolutionTest, ErrorEmptyInput)
+TEST_F(convolutionsTests, ErrorEmptyInput)
 {
     Eigen::ArrayXXd input = Eigen::ArrayXXd::Zero(0, 0);
     Eigen::ArrayXXd kernel = Eigen::ArrayXXd::Zero(3, 3);
     EXPECT_THROW(applyConvolution(input, kernel), std::invalid_argument);
 }
 
-TEST_F(applyConvolutionTest, ErrorEmptyKernel)
+TEST_F(convolutionsTests, ErrorEmptyKernel)
 {
     Eigen::ArrayXXd kernel = Eigen::ArrayXXd::Zero(0, 0);
     EXPECT_THROW(applyConvolution(input_eigen, kernel), std::invalid_argument);
 }
 
-TEST_F(applyConvolutionTest, ErrorKernelSize)
+TEST_F(convolutionsTests, ErrorKernelSize)
 {
     Eigen::ArrayXXd kernel = Eigen::ArrayXXd::Zero(2, 2);
     EXPECT_THROW(applyConvolution(input_eigen, kernel), std::invalid_argument);
 }
 
-TEST_F(applyConvolutionTest, ErrorKernelSquare)
+TEST_F(convolutionsTests, ErrorKernelSquare)
 {
     Eigen::ArrayXXd kernel = Eigen::ArrayXXd::Zero(3, 2);
     EXPECT_THROW(applyConvolution(input_eigen, kernel), std::invalid_argument);
 }
 
-TEST_F(applyConvolutionTest, ErrorKernelBiggerThanInput)
+TEST_F(convolutionsTests, ErrorKernelBiggerThanInput)
 {
     Eigen::ArrayXXd kernel = Eigen::ArrayXXd::Zero(6, 6);
     EXPECT_THROW(applyConvolution(input_eigen, kernel), std::invalid_argument);
