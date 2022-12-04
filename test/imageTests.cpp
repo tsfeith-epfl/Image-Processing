fbc8706c082d9b7a4d91e2124978dd8d0eebc0e2
tsfeith-epfl <tomas.soaresdecarvalhofeith@epfl.ch>
2022-12-04 12:07:30 +0100

    make imageTests consistent with latest commit

--- test/imageTests.cpp
+++ test/imageTests.cpp
@@ -58,55 +58,6 @@ TEST_F(imageTests, constructorWithNoDataAndValidParametersCreatesCorrectImage) {
     EXPECT_EQ(image.getWidth(), 1);
 }
 
-TEST_F(imageTests, constructorFromVectorDataWithParametersThrowsExceptionOnInvalidDimensions) {
-    EXPECT_THROW(Image(0, 0, 0, input_vector), std::invalid_argument);
-}
-
-TEST_F(imageTests, constructorFromVectorDataWithParametersThowsExceptionOnEmptyVector) {
-    vector<Eigen::ArrayXXd> empty_vector;
-    EXPECT_THROW(Image(1, 1, 1, empty_vector), std::invalid_argument);
-}
-
-TEST_F(imageTests, constructorFromVectorDataWithParametersThrowsExceptionOnInconsistentDimensions) {
-    EXPECT_THROW(Image(3, 3, 3, input_vector), std::invalid_argument);
-}
-
-TEST_F(imageTests, constructorFromVectorDataWithParametersThrowsExceptionOnInvalidData) {
-    EXPECT_THROW(Image(5, 5, 3, invalid_vector), std::invalid_argument);
-}
-
-TEST_F(imageTests, constructorFromVectorDataWithParamtersWorksWhenValid) {
-    Image image(5, 5, 3, input_vector);
-    EXPECT_EQ(image.getChannels(), 3);
-    EXPECT_EQ(image.getHeight(), 5);
-    EXPECT_EQ(image.getWidth(), 5);
-    EXPECT_EQ(image(0, 0, 0), 1);
-    EXPECT_EQ(image(1, 1, 1), 1);
-    EXPECT_EQ(image(2, 2, 2), 1);
-}
-
-TEST_F(imageTests, constructorFromEigenArrayWithParametersThrowsExceptionOnInvalidDimensions) {
-    EXPECT_THROW(Image(0, 0, 0, input_vector[0]), std::invalid_argument);
-}
-
-TEST_F(imageTests, constructorFromEigenArrayWithParametersThrowsExceptionOnInvalidData) {
-    EXPECT_THROW(Image(5, 5, 3, invalid_vector[0]), std::invalid_argument);
-}
-
-TEST_F(imageTests, constructorFromEigenArrayWithParamtersThrowsExceptionOnInconsistentDimensions) {
-    EXPECT_THROW(Image(3, 3, 3, input_vector[0]), std::invalid_argument);
-}
-
-TEST_F(imageTests, constructorFromEigenArrayWithParamtersWorksWhenValid) {
-    Image image(5, 5, 3, input_vector[0]);
-    EXPECT_EQ(image.getChannels(), 3);
-    EXPECT_EQ(image.getHeight(), 5);
-    EXPECT_EQ(image.getWidth(), 5);
-    EXPECT_EQ(image(0, 0, 0), 1);
-    EXPECT_EQ(image(1, 1, 1), 1);
-    EXPECT_EQ(image(2, 2, 2), 1);
-}
-
 TEST_F(imageTests, constructorFromEigenArrayWithChannelsThrowsExceptionOnInvalidDimensions) {
     EXPECT_THROW(Image(0, input_vector[0]), std::invalid_argument);
 }
@@ -183,7 +134,7 @@ TEST_F(imageTests, constructorFromVectorWithoutParametersWorksWhenValid) {
 }
 
 TEST_F(imageTests, constructorFromImageWorks) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     Image image_copy(image);
     EXPECT_EQ(image_copy.getChannels(), 3);
     EXPECT_EQ(image_copy.getHeight(), 5);
@@ -194,7 +145,7 @@ TEST_F(imageTests, constructorFromImageWorks) {
 }
 
 TEST_F(imageTests, assignmentOperatorWorks) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     Image image_copy = image;
     EXPECT_EQ(image_copy.getChannels(), 3);
     EXPECT_EQ(image_copy.getHeight(), 5);
@@ -205,22 +156,22 @@ TEST_F(imageTests, assignmentOperatorWorks) {
 }
 
 TEST_F(imageTests, getHeightWorks) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     EXPECT_EQ(image.getHeight(), 5);
 }
 
 TEST_F(imageTests, getWidthWorks) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     EXPECT_EQ(image.getWidth(), 5);
 }
 
 TEST_F(imageTests, getChannelsWorks) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     EXPECT_EQ(image.getChannels(), 3);
 }
 
 TEST_F(imageTests, getFullDataWorks) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     vector<Eigen::ArrayXXd> full_data = image.getData();
     EXPECT_EQ(full_data.size(), 3);
     EXPECT_EQ(full_data[0](0, 0), 1);
@@ -229,7 +180,7 @@ TEST_F(imageTests, getFullDataWorks) {
 }
 
 TEST_F(imageTests, getSingleChannelDataWorks) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     Eigen::ArrayXXd single_channel_data = image.getData(1);
     EXPECT_EQ(single_channel_data(0, 0), 1);
     EXPECT_EQ(single_channel_data(1, 1), 1);
@@ -237,17 +188,17 @@ TEST_F(imageTests, getSingleChannelDataWorks) {
 }
 
 TEST_F(imageTests, getSingleChannelDataThrowsExceptionOnInvalidChannel) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     EXPECT_THROW(image.getData(3), std::invalid_argument);
 }
 
 TEST_F(imageTests, getFullPixelWorksThrowsExceptionOnInvalidCoordinates) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     EXPECT_THROW(image.getPixel(5, 5), std::invalid_argument);
 }
 
 TEST_F(imageTests, getFullPixelWorks) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     Eigen::ArrayXd pixel = image.getPixel(2, 2);
     EXPECT_EQ(pixel.size(), 3);
     EXPECT_EQ(pixel[0], 1);
@@ -256,23 +207,23 @@ TEST_F(imageTests, getFullPixelWorks) {
 }
 
 TEST_F(imageTests, getSingleChannelPixelThrowsExceptionOnInvalidCoordinates) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     EXPECT_THROW(image.getPixel(5, 5, 1), std::invalid_argument);
 }
 
 TEST_F(imageTests, getSingleChannelPixelThrowsExceptionOnInvalidChannel) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     EXPECT_THROW(image.getPixel(2, 2, 3), std::invalid_argument);
 }
 
 TEST_F(imageTests, getSingleChannelPixelWorks) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     double pixel = image.getPixel(2, 2, 1);
     EXPECT_EQ(pixel, 1);
 }
 
 TEST_F(imageTests, setFullDataThrowsExceptionOnNewDataWithWrongChannels) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     vector<Eigen::ArrayXXd> new_data;
     new_data.emplace_back(Eigen::ArrayXXd::Zero(5, 5));
     new_data.emplace_back(Eigen::ArrayXXd::Zero(5, 5));
@@ -280,7 +231,7 @@ TEST_F(imageTests, setFullDataThrowsExceptionOnNewDataWithWrongChannels) {
 }
 
 TEST_F(imageTests, setFullDataThrowsExceptionOnNewDataWithWrongArrayDimensions) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     vector<Eigen::ArrayXXd> new_data;
     new_data.emplace_back(Eigen::ArrayXXd::Zero(5, 5));
     new_data.emplace_back(Eigen::ArrayXXd::Zero(5, 5));
@@ -289,12 +240,12 @@ TEST_F(imageTests, setFullDataThrowsExceptionOnNewDataWithWrongArrayDimensions) 
 }
 
 TEST_F(imageTests, setFullDataThrowsExceptionOnNewDataWithInvalidPixelValue) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     EXPECT_THROW(image.setData(invalid_vector), std::invalid_argument);
 }
 
 TEST_F(imageTests, setFullDataWorks) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     vector<Eigen::ArrayXXd> new_data;
     new_data.emplace_back(Eigen::ArrayXXd::Zero(5, 5));
     new_data.emplace_back(Eigen::ArrayXXd::Zero(5, 5));
@@ -306,22 +257,22 @@ TEST_F(imageTests, setFullDataWorks) {
 }
 
 TEST_F(imageTests, setSingleChannelDataThrowsExceptionOnNewDataWithWrongArrayDimensions) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     EXPECT_THROW(image.setData(1, Eigen::ArrayXXd::Zero(5, 6)), std::invalid_argument);
 }
 
 TEST_F(imageTests, setSingleChannelDataThrowsExceptionOnNewDataWithInvalidPixelValue) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     EXPECT_THROW(image.setData(1, invalid_vector[0]), std::invalid_argument);
 }
 
 TEST_F(imageTests, setSingleChannelDataThrowsExceptionOnInvalidChannel) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     EXPECT_THROW(image.setData(3, Eigen::ArrayXXd::Zero(5, 5)), std::invalid_argument);
 }
 
 TEST_F(imageTests, setSingleChannelDataWorks) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     image.setData(1, Eigen::ArrayXXd::Zero(5, 5));
     EXPECT_EQ(image(0, 0, 0), 1);
     EXPECT_EQ(image(1, 1, 1), 0);
@@ -329,24 +280,24 @@ TEST_F(imageTests, setSingleChannelDataWorks) {
 }
 
 TEST_F(imageTests, setFullPixelThrowsExceptionOnNewPixelWithWrongSize) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     EXPECT_THROW(image.setPixel(0, 0, Eigen::ArrayXd::Zero(2)), std::invalid_argument);
 }
 
 TEST_F(imageTests, setFullPixelThrowsExceptionOnNewPixelWithInvalidValue) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     Eigen::ArrayXd invalid_pixel(3);
     invalid_pixel << 1, 1, 2;
     EXPECT_THROW(image.setPixel(0, 0, invalid_pixel), std::invalid_argument);
 }
 
 TEST_F(imageTests, setFullPixelThrowsExceptionOnInvalidCoordinates) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     EXPECT_THROW(image.setPixel(5, 5, Eigen::ArrayXd::Zero(3)), std::invalid_argument);
 }
 
 TEST_F(imageTests, setFullPixelWorks) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     image.setPixel(0, 0, Eigen::ArrayXd::Zero(3));
     EXPECT_EQ(image(0, 0, 0), 0);
     EXPECT_EQ(image(0, 0, 1), 0);
@@ -354,22 +305,22 @@ TEST_F(imageTests, setFullPixelWorks) {
 }
 
 TEST_F(imageTests, setSingleChannelPixelThrowsExceptionOnNewPixelWithInvalidValue) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     EXPECT_THROW(image.setPixel(0, 0, 0, -1), std::invalid_argument);
 }
 
 TEST_F(imageTests, setSingleChannelPixelThrowsExceptionOnInvalidCoordinates) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     EXPECT_THROW(image.setPixel(5, 5, 0, 0), std::invalid_argument);
 }
 
 TEST_F(imageTests, setSingleChannelPixelThrowsExceptionOnInvalidChannel) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     EXPECT_THROW(image.setPixel(0, 0, 3, 0), std::invalid_argument);
 }
 
 TEST_F(imageTests, setSingleChannelPixelWorks) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     image.setPixel(0, 0, 0, 0);
     EXPECT_EQ(image(0, 0, 0), 0);
     EXPECT_EQ(image(0, 0, 1), 1);
@@ -377,12 +328,12 @@ TEST_F(imageTests, setSingleChannelPixelWorks) {
 }
 
 TEST_F(imageTests, fullChannelParenthesisOperatorThrowsExceptionOnInvalidChannel) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     EXPECT_THROW(image(3), std::invalid_argument);
 }
 
 TEST_F(imageTests, fullChannelParenthesisOperatorWorks) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     Eigen::ArrayXXd channel  = image(0);
     EXPECT_EQ(channel(0, 0), 1);
     EXPECT_EQ(channel(1, 1), 1);
@@ -390,12 +341,12 @@ TEST_F(imageTests, fullChannelParenthesisOperatorWorks) {
 }
 
 TEST_F(imageTests, fullPixelParenthesisOperatorThrowsExceptionOnInvalidCoordinates) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     EXPECT_THROW(image(5, 5), std::invalid_argument);
 }
 
 TEST_F(imageTests, fullPixelParenthesisOperatorWorks) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     Eigen::ArrayXd pixel = image(0, 0);
     EXPECT_EQ(pixel(0), 1);
     EXPECT_EQ(pixel(1), 1);
@@ -403,23 +354,23 @@ TEST_F(imageTests, fullPixelParenthesisOperatorWorks) {
 }
 
 TEST_F(imageTests, singleChannelPixelParenthesisOperatorThrowsExceptionOnInvalidCoordinates) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     EXPECT_THROW(image(5, 5, 0), std::invalid_argument);
 }
 
 TEST_F(imageTests, singleChannelPixelParenthesisOperatorThrowsExceptionOnInvalidChannel) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     EXPECT_THROW(image(0, 0, 3), std::invalid_argument);
 }
 
 TEST_F(imageTests, singleChannelPixelParenthesisOperatorWorks) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     double pixel = image(0, 0, 0);
     EXPECT_EQ(pixel, 1);
 }
 
 TEST_F(imageTests, conversionToCvMatWorks) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     cv::Mat cv_image = image.toCvMat();
     EXPECT_EQ(cv_image.rows, 5);
     EXPECT_EQ(cv_image.cols, 5);
@@ -427,6 +378,58 @@ TEST_F(imageTests, conversionToCvMatWorks) {
 }
 
 TEST_F(imageTests, saveImageWorks) {
-    Image image(5, 5, 3, input_vector);
+    Image image(input_vector);
     image.save("test_image.png");
+}
+
+TEST_F(imageTests, equalityOperatorReturnsTrueForEqualImages) {
+    Image image1(input_vector);
+    Image image2(input_vector);
+    EXPECT_TRUE(image1 == image2);
+}
+
+TEST_F(imageTests, equalityOperatorReturnsFalseForImagesWithDifferentValues) {
+    Image image1(input_vector);
+    Image image2(input_vector);
+    image2.setPixel(0, 0, 0, 0);
+    EXPECT_FALSE(image1 == image2);
+}
+
+TEST_F(imageTests, equalityOperatorReturnsFalseForImagesWithDifferentDimensions) {
+    Image image1(input_vector);
+    Image image2(input_vector[0]);
+    EXPECT_FALSE(image1 == image2);
+}
+
+TEST_F(imageTests, differenceOperatorReturnsTrueForDifferentImages) {
+    Image image1(input_vector);
+    Image image2(input_vector);
+    image2.setPixel(0, 0, 0, 0);
+    EXPECT_TRUE(image1 != image2);
+}
+
+TEST_F(imageTests, differenceOperatorReturnsFalseForEqualImages) {
+    Image image1(input_vector);
+    Image image2(input_vector);
+    EXPECT_FALSE(image1 != image2);
+}
+
+TEST_F(imageTests, differenceOperatorReturnsTrueForImagesWithDifferentDimensions) {
+    Image image1(input_vector);
+    Image image2(input_vector[0]);
+    EXPECT_TRUE(image1 != image2);
+}
+
+TEST_F(imageTests, reduceChannelslreturnsSingleChannelImageUnchanged) {
+    Image image(input_vector[0]);
+    Image reduced_image = image.reduceChannels();
+    EXPECT_EQ(reduced_image, image);
+}
+
+TEST_F(imageTests, reduceChannelreturnsImageWithReducedChannels) {
+    Image image(input_vector);
+    Image reduced_image = image.reduceChannels();
+    EXPECT_EQ(reduced_image.getWidth(), 5);
+    EXPECT_EQ(reduced_image.getHeight(), 5);
+    EXPECT_EQ(reduced_image.getChannels(), 1);
 }
