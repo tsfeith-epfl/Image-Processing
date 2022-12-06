2837607eb50c1e0d33c19d06eecc10fd2daa74cf
tsfeith-epfl <tomas.soaresdecarvalhofeith@epfl.ch>
2022-12-06 10:58:52 +0100

    fix setup and build to use jsoncpp

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
-cd libraries
 
+# go to libraries and download the necessary ones
+cd libraries
 # download eigen
 git clone https://gitlab.com/libeigen/eigen.git
-
+# install jsoncpp
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    sudo apt-get install libjsoncpp-dev
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    brew install jsoncpp
+fi
 # install opencv
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     sudo apt-get install -y libopencv-dev
@@ -11,7 +17,6 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
 fi
 
 # install and build googletest (for unit tests)
-cd ..
 git clone https://github.com/google/googletest.git
 cd googletest
 mkdir build
