9c7d5d7bcf5b0eb9edb3f71bf52557a54073eed9
andrecharneca-epfl <andre.charnecaduarte@epfl.ch>
2022-12-12 15:53:23 +0100

    small changes

--- andre.cpp
+++ andre.cpp
@@ -11,23 +11,30 @@
 #include "ContourExtractor.hpp"
 #include "Histogram.hpp"
 #include "parameters.hpp"
+#include <fstream>
 
 using namespace std;
 
 int main(){
     // read image
-    Image image("pinwheel.png");
+    Image image("tiger.png");
+    Image gray = image.reduceChannels();
 
-    // create contour extractor
-    ContourExtractor contourExtractor(0.3, 5, 2);
-
-    // extract contours
-    //Image contours = contourExtractor.extractContours(image, true);
+    // count percentage of black pixels
+    int black_pixels = 0;
+    for (int i = 0; i < gray.getWidth(); i++){
+        for (int j = 0; j < gray.getHeight(); j++){
+            if (gray(i, j, 0) == 0){
+                black_pixels++;
+            }
+        }
+    }
+    double percentage = (double) black_pixels / (gray.getHeight() * gray.getWidth());
+    cout << "Percentage of black pixels: " << percentage << endl;
 
     // histogram
     Histogram histogram(HISTOGRAM_BINS, HISTOGRAM_MIN, HISTOGRAM_MAX, LOG_SCALE);
-    histogram.getHistogram(image);
+    histogram.getHistogram(image, true);
     return 0;
 }
-//Q: How to use default arguments of functions in c++?
-//A:
+
