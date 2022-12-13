64d347d3ab6b965a7c375a0ce414e3764e387a1a
andrecharneca-epfl <andre.charnecaduarte@epfl.ch>
2022-12-13 21:19:29 +0100

    small change

--- src/FourierImage.hpp
+++ src/FourierImage.hpp
@@ -1,12 +1,18 @@
 //
 // Created by André Charneca on 12.12.22.
 //
+// Child of Image class. Contains methods to perform Fourier Transformations on the image.
 
 #ifndef IMAGEPROCESSING_FOURIERIMAGE_HPP
 #define IMAGEPROCESSING_FOURIERIMAGE_HPP
 
+#include "Image.hpp"
 
-class FourierImage {
+class FourierImage : public Image {
+private:
+    vector<Eigen::ArrayXXcd> transform_data;
+
+public:
 
 };
 
