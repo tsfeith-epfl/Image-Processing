0e566c48b371bb7d84ef2ad56b51bb404972e02c
andrecharneca-epfl <andre.charnecaduarte@epfl.ch>
2022-12-15 14:24:53 +0100

    added Fourier Transform params

--- parameters.hpp
+++ parameters.hpp
@@ -5,6 +5,8 @@
 #ifndef IMAGEPROCESSING_PARAMETERS_HPP
 #define IMAGEPROCESSING_PARAMETERS_HPP
 
+#include <string>
+
 /* Denoiser Parameters */
 double DENOISER_SIGMA = 1;
 int DENOISER_KERNEL_SIZE = 3;
@@ -19,5 +21,12 @@ int HISTOGRAM_BINS = 500;
 double HISTOGRAM_MIN = 0.;
 double HISTOGRAM_MAX = 1;
 bool LOG_SCALE = false;
+
+/* Fourier Transform Parameters */
+bool SHOW_FOURIER_PROGRESS = true;
+bool SHOW_FOURIER_LOG_MAGNITUDE = true;
+double LOW_CUTOFF = 0.5;
+double HIGH_CUTOFF = 0.9;
+string FILTER_TYPE = "low"; // "band", "low", "high"
 
 #endif //IMAGEPROCESSING_PARAMETERS_HPP
